ea4d774be498b2ac882d1f4089650919cfffa3b4
Chiara Gerlach <chiara.gerlach@online.de>
2020-08-26 09:57:29 +0200

    added validations to the booking model and continued working on the interface

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
+
+  validates :delivery_date, presence: true
+  validates :delivery_time, presence: true
+  validates :delivery_address, presence: true
+  validates :status, presence: true
+  validates :floor, presence: true
+
 end
