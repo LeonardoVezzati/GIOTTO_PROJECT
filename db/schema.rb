53981f8b5e4c52f5cb2962912a1c5f64d6f58183
leonardo vezzati <leonardo.vezzati@gmail.com>
2020-08-26 11:38:40 +0200

    Index with pictures

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_145416) do
+ActiveRecord::Schema.define(version: 2020_08_25_210104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "bookings", force: :cascade do |t|
     t.string "status"
@@ -37,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_08_24_145416) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "category"
+    t.string "photo"
   end
 
   create_table "packages", force: :cascade do |t|
@@ -47,6 +69,14 @@ ActiveRecord::Schema.define(version: 2020_08_24_145416) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["booking_id"], name: "index_packages_on_booking_id"
     t.index ["furniture_id"], name: "index_packages_on_furniture_id"
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "photo"
+    t.bigint "furniture_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["furniture_id"], name: "index_pictures_on_furniture_id"
   end
 
   create_table "teams", force: :cascade do |t|
@@ -69,7 +99,9 @@ ActiveRecord::Schema.define(version: 2020_08_24_145416) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "users"
   add_foreign_key "packages", "bookings"
   add_foreign_key "packages", "furnitures"
+  add_foreign_key "pictures", "furnitures"
 end
