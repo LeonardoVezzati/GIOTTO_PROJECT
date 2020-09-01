081a3104b30336d391d47143e4f4e24920e2e28c
Nicolas Emmery <nicolas.emmery@gmail.com>
2020-09-01 17:55:46 +0200

    card display info

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,8 +2,9 @@ class TeamsController < ApplicationController
 
   def index
     @current_user_team = User.all.filter { |user| user.team_id == current_user.team_id }
-    @team = current_user.team
-    @total_members= @team.users.count
+    @team = current_user.team.users.map { |user| { user: user, expenses: user.packages.map{ |pack| pack.price }.sum }}
+    @total_members= current_user.team.users.count
+
   end
 
   def packages
@@ -16,7 +17,7 @@ class TeamsController < ApplicationController
       redirect_to teams_path
       flash[:alert] = 'Sorry, no access to that user.'
     end
-  
+
   end
 
   def update
@@ -26,6 +27,36 @@ class TeamsController < ApplicationController
   end
 
   private
+
+  def price(user, pack)
+    divide = 0
+    if user.team.rental_period == 1
+      divide = 0.20
+    elsif user.team.rental_period == 2
+      divide = 0.125
+    elsif user.team.rental_period == 3
+      divide = 0.0933
+    elsif user.team.rental_period == 4
+      divide = 0.0775
+    elsif user.team.rental_period == 5
+      divide = 0.068
+    elsif user.team.rental_period == 6
+      divide = 0.06
+    elsif user.team.rental_period == 7
+      divide = 0.0543
+    elsif user.team.rental_period == 8
+      divide = 0.0513
+    elsif user.team.rental_period == 9
+      divide = 0.0489
+    elsif user.team.rental_period == 10
+      divide = 0.047
+    elsif user.team.rental_period == 11
+      divide = 0.0445
+    else
+      divide = 0.0417
+    end
+    (((pack.furniture.price_cents * divide) * pack.number)/100).ceil
+  end
 
   def params_team
     params.require(:team).permit(:rental_period)
