a305098a3deb73f5955b9a4a128c72f4a5e3d76c
leonardo vezzati <leonardo.vezzati@gmail.com>
2020-09-02 08:15:42 +0200

    meta, favicon and errors

--- app/models/package.rb
+++ app/models/package.rb
@@ -2,8 +2,8 @@ class Package < ApplicationRecord
   belongs_to :furniture
   belongs_to :booking, optional: true
   belongs_to :user
-  validates :number, presence: true
-
+  validates :number, presence: true, numericality: { greater_than: 0 }
+  
   validate :under_budget
   def under_budget
     total = Package.total(user)
