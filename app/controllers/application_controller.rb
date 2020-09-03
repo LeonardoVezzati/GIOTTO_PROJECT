b00062522a76da05e8394c1b36f41c473003a125
leonardo vezzati <leonardo.vezzati@gmail.com>
2020-09-03 16:47:56 +0200

    Sign up fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:team_id])
     devise_parameter_sanitizer.permit(:invite, keys: [:firstname, :lastname, :budget_per_month, :team_id])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:status])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:budget_per_month,:status])
     devise_parameter_sanitizer.permit(:account_update, keys: [:budget_per_month, :firstname, :lastname, :picture])
   end
 
